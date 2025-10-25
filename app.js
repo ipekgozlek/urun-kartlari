de2b4240dd995ff43453455a72fe04ca76e7a40c
İpek Gözlek <ipekgozlek@gmail.com>
2025-10-25 23:19:31 +0300

    banner ve logo düzenlendi

--- app.js
+++ app.js
@@ -1,17 +1,11 @@
 
   let products = [
-      { id: 1, name: "Lacivert Gömlek", price: 1999.9, 
-        image: "https://cdn2.jimmykey.com/jimmykey/ContentImages/Product/2025kis/5WW035633/bluz_5ww035633_lacivert-494-lacivert_1_614x805.webp" },
-      { id: 2, name: "Kırmızı Kısa Kollu Gömlek",  price: 1499.9, 
-         image: "https://cdn2.jimmykey.com/jimmykey/ContentImages/Product/2025kis/5WW035264/bluz_5ww035264_kirmizi-600-kirmizi_1_614x805.webp" },
-      { id: 3, name: "Leopar Desenli Gömlek",   price: 2999.9, 
-         image: "https://cdn2.jimmykey.com/jimmykey/ContentImages/Product/2025kis/5WW085612/gomlek_5ww085612_aci-kahve-803-kahverengi_1_614x805.webp" },
-       { id:4,name:"Mavi Örme Midi Elbise",price:2499.9,
-        image:"https://cdn2.jimmykey.com/jimmykey/ContentImages/Product/2025kis/5WW065870/elbise_5ww065870_koyu-petrol-427-koyu-yesil_1_614x805.webp"},
-        {id:5,name:"Fiyonk Detaylı Dokuma Mini Elbise",price:799.9,
-          image:"https://cdn2.jimmykey.com/jimmykey/ContentImages/Product/2025yaz/5SW064409/elbise_5sw064409_ekru-114-ekru_3_614x805.webp"},
-        {id:6,name:"Boncuk Detaylı Askılı Mini Elbise",price:1499.9,
-          image:"https://cdn2.jimmykey.com/jimmykey/ContentImages/Product/2025yaz/5SW063118/100-keten-duz-kesim-boncuk-detayli-askili-mini-elbise_5sw063118_aci-kahve-803-kahverengi_8_614x805.webp"
+      { id: 1, name: "Lacivert Gömlek", price: 1999.9,  image: "https://cdn2.jimmykey.com/jimmykey/ContentImages/Product/2025kis/5WW035633/bluz_5ww035633_lacivert-494-lacivert_1_614x805.webp" },
+      { id: 2, name: "Kırmızı Kısa Kollu Gömlek",  price: 1499.9, image: "https://cdn2.jimmykey.com/jimmykey/ContentImages/Product/2025kis/5WW035264/bluz_5ww035264_kirmizi-600-kirmizi_1_614x805.webp" },
+      { id: 3, name: "Leopar Desenli Gömlek",   price: 2999.9, image: "https://cdn2.jimmykey.com/jimmykey/ContentImages/Product/2025kis/5WW085612/gomlek_5ww085612_aci-kahve-803-kahverengi_1_614x805.webp" },
+       { id:4,name:"Mavi Örme Midi Elbise",price:2499.9, image:"https://cdn2.jimmykey.com/jimmykey/ContentImages/Product/2025kis/5WW065870/elbise_5ww065870_koyu-petrol-427-koyu-yesil_1_614x805.webp"},
+        {id:5,name:"Fiyonk Detaylı Dokuma Mini Elbise",price:799.9,image:"https://cdn2.jimmykey.com/jimmykey/ContentImages/Product/2025yaz/5SW064409/elbise_5sw064409_ekru-114-ekru_3_614x805.webp"},
+        {id:6,name:"Boncuk Detaylı Askılı Mini Elbise",price:1499.9,image:"https://cdn2.jimmykey.com/jimmykey/ContentImages/Product/2025yaz/5SW063118/100-keten-duz-kesim-boncuk-detayli-askili-mini-elbise_5sw063118_aci-kahve-803-kahverengi_8_614x805.webp"
         } 
     ];
 
@@ -20,67 +14,18 @@
 localStorage.setItem("products",JSON.stringify(products));
 let localProducts = JSON.parse(localStorage.getItem("products"));
 
-
-
 let container = document.getElementById("product-list");
-
-
-
-
-
 let carouselInner=document.getElementById("carousel-indicators");
 
 localProducts.forEach(p => {
-  // Dış kolon div
-  let col = document.createElement("div");
-  col.className = "col-md-2";
-  // col-md-2 mb-2
 
-  // Kart div
-  let card = document.createElement("div");
-  card.className = "card h-100 shadow-sm";
-
-  // Görsel
-  let img = document.createElement("img");
-  img.src = p.image;
-  img.alt = p.name;
-  img.className = "card-img-top";
-
-  // Kart gövdesi
-  let body = document.createElement("div");
-  body.className = "card-body text-center";
-
-  let title = document.createElement("h5");
-  title.className = "card-title";
-  title.textContent = p.name;
-
-  let price = document.createElement("p");
-  price.className = "card-text text-danger fw-bold";
-  price.textContent = `${p.price.toFixed(2)} TL`;
-
-  let addButton=document.createElement("button");
-  addButton.className="btn btn-success mt-2";
-  addButton.innerText="Sepete Ekle";
-
-  addButton.addEventListener("click", () => {
-    alert(`${p.name} Bu ürün sepetinize eklendi`);
   });
 
-  // Birleştirme
-  body.appendChild(title);
-  body.appendChild(price);
-  body.appendChild(addButton);
-  card.appendChild(img);
-  card.appendChild(body);
-  col.appendChild(card);
-  container.appendChild(col);
-
-  // carouselInner.appendChild(item);
-
-}
 
 
-);
+
+
+
 
 
 const button = document.createElement("a");
@@ -98,9 +43,3 @@ divSec.appendChild(button);
 //   "ButtonDiv2"
 // ).innerHTML = `<a id="denemeButton2" class="btn btn-danger" href="https://www.google.com.tr" target="_blank">Bana da Tıkla</a>`;
 
-
-let total=0;
-localProducts.forEach(p =>{
-     total +=p.price;
-});
-document.getElementById("totalPrice").innerText = total.toFixed(2) + "TL";
